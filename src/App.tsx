4d8bfeac4bf74a7b752d4054c39e4ef9a09bc296
Gabriel Brito <ferreirabritogabriel@gmail.com>
2022-05-03 17:14:01 -0300

    Add MirageJS - Api fake

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Header } from './components/Header/indesx';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 import { Dashboard } from './components/Dashboard';
+
 export function App() {
 	return (
 		<div className="App">
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 export function TransactionTable() {
+	useEffect(() => {
+		fetch('http://localhost:3000/api/transactions')
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	}, []);
+
 	return (
 		<Container>
 			<table>
 				<thead>
 					<tr>
-						<th>Titulo</th>
+						<th>Título</th>
 						<th>Valor</th>
 						<th>Categoria</th>
 						<th>Data</th>
